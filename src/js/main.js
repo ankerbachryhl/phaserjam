8a19a4167018316247fe27a10fd739da6156202b
Anker Bach Ryhl <ankerbachryhl@gmail.com>
2016-11-26 21:03:33 +0100

    Added controls

--- src/js/main.js
+++ src/js/main.js
@@ -1,211 +1,119 @@
+
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
 function preload() {
 
-    game.load.image('sky', '../images/sky.png');
-    game.load.image('ground', '../images/platform.png');
-    game.load.image('star', '../images/star.png');
-    game.load.image('cat_evovle2', '../images/cat_evovle2.png');
-    game.load.spritesheet('dude', '../images/dude.png', 32, 48);
-
-    game.load.image('bird', '../images/bird.png', 30, 30);
-
+    //Map
+    game.load.tilemap('mario', '../levels/level3.json', null, Phaser.Tilemap.TILED_JSON)
+    game.load.image('tiles', '../images/tilesheet.png')
+    game.load.spritesheet('dude', '../images/40x40-square.png', 40, 40);
 }
 
+
+var map;
+var layer;
 var player;
-var platforms;
 var cursors;
-
-var stars;
-var score = 0;
-var scoreText;
-
-var enemy;
-var bird;
+var jumpCount;
+var jumpkey;
 
 function create() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.stage.backgroundColor = '#787878';
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+  //  The 'mario' key here is the Loader key given in game.load.tilemap
+  map = game.add.tilemap('mario');
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+  //  The first parameter is the tileset name, as specified in the Tiled map editor (and in the tilemap json file)
+  //  The second parameter maps this name to the Phaser.Cache key 'tiles'
+  map.addTilesetImage('tiles', 'tiles');
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+  //  Creates a layer from the World1 layer in the map data.
+  //  A Layer is effectively like a Phaser.player, so is added to the display list.
+  layer = map.createLayer('World1');
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+  //  This resizes the game world to match the layer dimensions
+  layer.resizeWorld();
+  map.setCollisionBetween(0, 4, true, 'World1');
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // enemy = game.add.sprite(20, 0, 'bird')
-    // game.physics.arcade.enable(enemy);
-    // enemy.enableBody = true;
-    // enemy.body.gravity.y = 300;
+  //Before you can use the collide function you need to set what tiles can collide
 
 
+  //Add the player to the game and enable arcade physics on it
+  player = game.add.sprite(50, game.world.centerY, 'dude');
+  game.physics.arcade.enable(player);
 
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+  //Change the world size to match the size of this layer
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
+  //Set some physics on the player
+  player.body.bounce.y = 0.2;
+  player.body.gravity.y = 300;
+  player.body.velocity.x = 50;
+  player.body.collideWorldBounds = true;
 
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-    //  Finally some stars to collect
-    stars = game.add.group();
+  //Create a running animation for the player and play i
 
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
+  //Make the camera follow the player
+  game.camera.follow(player);
 
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
+  //Enable cursor keys so we can create some controls
+  cursors = game.input.keyboard.createCursorKeys();
 
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
 
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
 
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-
-    bird = game.add.sprite(100, 400, 'bird')
-    bird.anchor.setTo(0.5, 9.5)
-
-    game.physics.enable(bird, Phaser.Physics.ARCADE);
-    bird.body.immovable = true;
-    bird.body.collideWorldBounds = true;
-
-    bird.body.allowGravity = false;
-
-    birdTween = game.add.tween(bird).to({
-      y: bird.y + 200
-    }, 2000, 'Linear', true, 0, 150, true);
 
 }
 
 function update() {
 
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.collide(enemy, platforms);
+  player.body.velocity.x = 0;
 
-    if (game.physics.arcade.collide(player, enemy)) {
-      score -= 10;
-      scoreText.text = 'Score: ' + score;
-    }
+  if (cursors.left.isDown)
+  {
+      //  Move to the left
+      player.body.velocity.x = -150;
 
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+      player.loadTexture('cat_left', 0)
+  }
+  else if (cursors.right.isDown)
+  {
+      //  Move to the right
+      player.body.velocity.x = 150;
 
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
+      player.animations.play('right');
+  }
+  else
+  {
+      //  Stand still
+      player.animations.stop();
 
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
+      player.frame = 4;
+  }
 
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
+  //  Allow the player to jump if they are touching the ground.
+  if (cursors.up.isDown && player.body.touching.down)
+  {
+      player.body.velocity.y = -350;
+  }
 
-        player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
 
-        player.frame = 4;
-    }
 
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
+  game.physics.arcade.collide(player, layer);
 
-    if (score > 30) {
-      evovle2()
-    }
+  //Make the player jump when the up key is pushed
 
-    if (score < 30) {
-      evovle1()
-    }
+  if (cursors.up.isDown && player.body.onFloor())
 
-    //Enemy overlap
+      {
+          player.body.velocity.y = -350;
+      }
 
-    if (checkOverlap(player, bird)) {
-      resetPlayer()
-    }
 
-}
 
-function collectStar (player, star) {
+  //tells phaser to fire jumpCheck() ONCE per onDown event.jumpCheck = function(){   if (player.jumpCount < 2){      player.jump();      player.jumpCount ++;   }}
 
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-
-}
-
-function evovle2() {
-  player.loadTexture('cat_evovle2', 0)
-}
-
-function evovle1() {
-  player.loadTexture('dude', 0)
-}
-
-//Bird
-
-function checkOverlap(spriteA, spriteB) {
-  var boundsA = spriteA.getBounds();
-  var boundsB = spriteB.getBounds();
-
-  return Phaser.Rectangle.intersects(boundsA, boundsB)
-
-}
-
-function resetPlayer() {
-  player.reset(0, 450)
 }
